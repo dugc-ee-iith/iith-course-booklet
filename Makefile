68ce05762de139927f75dba6b2338571513b4f21
Shishir Kumar <shishirk@iith.ac.in>
2019-03-08 13:28:00 +0530

    Added functionality to print course description from courses.db directly. python3 main.py print-desc ee Started work on printing curriculum. It will need database of all departments to work properly. Many edge cases "EEXXXX" "XXXXXX" need to be considered. courses.db should be standardized. python3 main.py print-curr ee # currently uses ee.xlsx Made style files for curriculum. Both the style files need to have only table styling. Or it may be better to have all table related styling in the print functions itself. Document wide styling can remain in the files itself. Current style if not great, I have to think of better way to present this data.

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ test:main.py styles/*.sty
 	python3 main.py > test.tex
 	lualatex test.tex
 
+clean:
+	rm test.pdf test.log test.aux 
--- main.py
+++ main.py
@@ -10,6 +10,17 @@ import csv
 from openpyxl import load_workbook
 import sqlite3
 
+def get_course_db(dept):
+    con = sqlite3.connect('./courses.db')
+    cur = con.cursor()
+    #check if it is in the db already
+    chk = """SELECT name FROM sqlite_master WHERE type='table' AND name='%s_courses';""" % dept
+    chk_res = cur.execute(chk).fetchall()
+    if len(chk_res)==0: 
+        print("Table %s_courses not found, please upload..." % dept)
+        sys.exit()
+    return cur
+
 def update_dept_cdesc(dept, sheet):
     #use like this: update_dept_cdesc('ee', wb.get_sheet_by_name("course-descriptions"))
     con = sqlite3.connect('./courses.db')
@@ -48,8 +59,6 @@ def update_dept_cdesc(dept, sheet):
     con.commit()
     con.close()
 
-wb = load_workbook("EE.xlsx")
-    
 def csv2latex():
     pre = open("./styles/descript_pre.sty").readlines()
     post = open("./styles/descript_post.sty").readlines()
@@ -70,27 +79,93 @@ def csv2latex():
     #row_style = "\\colchunk{ \\describe{%s}{%s}{%s}{%s} } \\colchunk{ \\syllabus{%s} }"
     #row_post = "\\end{parcolumns}"
 
-    row_pre = "\\columnratio{0.2} \\setlength{\\columnsep}{1em} \\begin{paracol}{2}"
-    print(row_pre)
+    table_pre = "\\columnratio{0.2} \\setlength{\\columnsep}{1em} \\begin{paracol}{2}"
+    print(table_pre)
     row_style = "\\describe{%s}{%s}{%s}{%s} \\switchcolumn \\vspace{3mm} \\syllabus{%s} \\switchcolumn[0]*"
     for r in desc.iter_rows(min_row=2):
         code, credits, segments, name, syl = r[1].value, r[3].value, r[4].value, r[2].value, r[6].value
         if segments == None or segments == 'None': segments = ''
         name = name.replace('&', 'and')
         syl = syl.replace('\\', '').replace('&', 'and')
-        try:
-            print(row_style % (code, credits, segments, name, syl))
-        except:
-            pass
+        try: print(row_style % (code, credits, segments, name, syl))
+        except: pass
     
     #table_post = "\\bottomrule \\end{tabular} \\end{table*}"
     #table_post = "\\bottomrule \\end{longtable}"
     #print(table_post)
-    row_post = "\\end{paracol}"
-    print(row_post)
+    table_post = "\\end{paracol}"
+    print(table_post)
     print("".join(post))
 
-csv2latex()
+def gen_course_description(dept):
+    pre = open("./styles/descript_pre.sty").readlines()
+    post = open("./styles/descript_post.sty").readlines()
+    print("".join(pre))
 
+    cur = get_course_db(dept)
+    table_pre = "\\columnratio{0.2} \\setlength{\\columnsep}{1em} \\begin{paracol}{2}"
+    print(table_pre)
+    row_style = "\\describe{%s}{%s}{%s}{%s} \\switchcolumn \\vspace{3mm} \\syllabus{%s} \\switchcolumn[0]*"
+    q = """SELECT code, credits, segments, name, syllabus FROM %s_courses""" % dept
+    rows = cur.execute(q).fetchall()
+    for row in rows: 
+        code, credits, segments, name, syl = row
+        if segments == None or segments == 'None': segments = ''
+        name = name.replace('&', 'and')
+        syl = syl.replace('\\', '').replace('&', 'and')
+        print(row_style % (code, credits, segments, name, syl))
+    table_post = "\\end{paracol}"
+    print(table_post)
+    print("".join(post))
+
+def gen_curriculum(dept, sheet, title):
+    pre = open("./styles/curriculum_pre.sty").readlines()
+    post = open("./styles/curriculum_post.sty").readlines()
+    print("".join(pre))
+    
+    cur = get_course_db(dept)
+    table_pre = "\\begin{longtable}{@{}llll@{}} \
+    \\textcolor{Grey}{\\textbf{Course Code}} & \\textcolor{Grey}{\\textbf{Course Name}} & \
+    \\textcolor{Grey}{\\textbf{Credits}} & \\textcolor{Grey}{\\textbf{Segments}} \\\\ \
+    \\toprule \\endhead"
+    print(table_pre)
+    row_style = "\\currformat{%s}{%s}{%s}{%s}"
+    q = """SELECT credits, segments, name FROM %s_courses WHERE code='%s'"""
+    sem, t1, t2 = "0", 0, 0 # t1 = total credits, t2 = credits for a semester.
+    for r in sheet.iter_rows(min_row=2):
+        if sem != r[0].value:
+            if sem != "0": print(" & Total & %d & \\\\"%t2)
+            sem, t2 = r[0].value, 0
+            print(" & Semester %s & & \\\\" % sem)
+        code = r[1].value
+        dept = code[:2].lower()
+        #print(code, dept)
+        try: credits, segments, name = cur.execute(q % (dept, code)).fetchall()[0]
+        except: continue
+        t1 += int(credits)
+        t2 += int(credits)
+        if segments == None or segments == 'None': segments = ''
+        name = name.replace('&', 'and')
+        print(row_style % (code, name, credits, segments))
+    table_post = "\\end{longtable}"
+    print(table_post)
+    print("".join(post))
+
+
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "update":
+            wb = load_workbook(sys.argv[2])
+            desc = wb.get_sheet_by_name("course-descriptions")
+            update_dept_cdesc(sys.argv[3], sheet)
+        elif sys.argv[1] == "print-desc":
+            gen_course_description(sys.argv[2])
+        elif sys.argv[1] == "print-curr":
+            wb = load_workbook("%s.xlsx" % sys.argv[2])
+            sheet = wb.get_sheet_by_name("curriculum")
+            gen_curriculum(sys.argv[2], sheet, "UG Curriculum")
+    
+    
 
 
