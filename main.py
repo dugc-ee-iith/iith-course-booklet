923f51a2a44668c7a61b9991996c7aa645a538c2
Shishir Kumar <shishirk@iith.ac.in>
2019-03-15 13:59:07 +0530

    Added data files, and reached base version of the document. Several changes in main.py. Now data has to be cleaned so that uniform formatting is used.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import json
 import csv
 
+from os import walk
+course_details_filelist = ['./data/UG/'+y for y in [x[2] for x in walk('./data/UG')][0]]
+#print(course_details_filelist)
+
 #book = xlrd.open_workbook('test-dat1.xls')
 #sh1 = book.sheet_by_index(0)
 
@@ -30,18 +34,19 @@ def update_dept_cdesc(dept, sheet):
     chk_res = cur.execute(chk).fetchall()
     if len(chk_res)==0: 
         print("Table %s_courses not found, creating..." % dept)
-    # create table
+        # create table
+        # Code is unique, name is not, as same name can be offered for UG and PG.
         c = """
-    CREATE TABLE %s_courses (
-    code           STRING  PRIMARY KEY UNIQUE NOT NULL,
-    name           STRING  UNIQUE NOT NULL,
-    credits        INTEGER NOT NULL,
-    semester       INTEGER,
-    pre_req        STRING,
-    syllabus       TEXT,
-    segments       STRING,
-    remarks        TEXT,
-    global_remarks TEXT
+        CREATE TABLE %s_courses (
+        code           STRING  PRIMARY KEY UNIQUE NOT NULL,
+        name           STRING  NOT NULL,
+        credits        INTEGER NOT NULL,
+        semester       INTEGER,
+        pre_req        STRING,
+        syllabus       TEXT,
+        segments       STRING,
+        remarks        TEXT,
+        global_remarks TEXT
         );""" % dept
         cur.execute(c)
         con.commit()
@@ -53,9 +58,12 @@ def update_dept_cdesc(dept, sheet):
         chk_res = cur.execute(chk, [r[1].value]).fetchall()
         if len(chk_res)==0:
             try:
-                cur.execute(ins, [r[0].value, r[1].value, r[2].value, int(r[3].value), r[4].value,
-                        r[5].value, r[6].value, r[7].value, r[8].value])
-            except: print(r[1].value)
+                cur.execute(ins, [r[0].value, r[1].value.upper(), r[2].value, 
+                    int(r[3].value), r[4].value, r[5].value, r[6].value,
+                    r[7].value, r[8].value])
+            except Exception as e: 
+                print(r, r[1].value, r[2].value)
+                print(e)
     con.commit()
     con.close()
 
@@ -98,10 +106,9 @@ def csv2latex():
     print("".join(post))
 
 def gen_course_description(dept):
-    pre = open("./styles/descript_pre.sty").readlines()
-    post = open("./styles/descript_post.sty").readlines()
-    print("".join(pre))
-
+    #pre = open("./parts/pre_descr_table.tex").readlines()
+    #post = open("./parts/post_descr_table.tex").readlines()
+    #print("".join(pre))
     cur = get_course_db(dept)
     table_pre = "\\columnratio{0.2} \\setlength{\\columnsep}{1em} \\begin{paracol}{2}"
     print(table_pre)
@@ -112,59 +119,101 @@ def gen_course_description(dept):
         code, credits, segments, name, syl = row
         if segments == None or segments == 'None': segments = ''
         name = name.replace('&', 'and')
-        syl = syl.replace('\\', '').replace('&', 'and')
+        #if syl in not None: syl = syl.replace('\\', '').replace('&', 'and')
+        if syl is not None: 
+            syl = syl.replace('&', 'and').replace('\\', '\\textbackslash ')
         print(row_style % (code, credits, segments, name, syl))
     table_post = "\\end{paracol}"
     print(table_post)
-    print("".join(post))
+    #print("".join(post))
 
 def gen_curriculum(dept, sheet, title):
-    pre = open("./styles/curriculum_pre.sty").readlines()
-    post = open("./styles/curriculum_post.sty").readlines()
-    print("".join(pre))
-    
+    #pre = open("./parts/pre_curr_table.tex").readlines()
+    #post = open("./parts/post_curr_table.tex").readlines()
+    #print("".join(pre))
     cur = get_course_db(dept)
-    table_pre = "\\begin{longtable}{@{}llll@{}} \
-    \\textcolor{Grey}{\\textbf{Course Code}} & \\textcolor{Grey}{\\textbf{Course Name}} & \
-    \\textcolor{Grey}{\\textbf{Credits}} & \\textcolor{Grey}{\\textbf{Segments}} \\\\ \
-    \\toprule \\endhead"
+    #table_pre = "\\begin{longtable}{@{}llll@{}} \
+    #\\textcolor{Grey}{\\textbf{Course Code}} & \\textcolor{Grey}{\\textbf{Course Name}} & \
+    #\\textcolor{Grey}{\\textbf{Credits}} & \\textcolor{Grey}{\\textbf{Segments}} \\\\ \
+    #\\toprule \\endhead"
+    #{@{}rlll@{}} \
+    #table_pre = """\\setlength\\LTleft{0pt} \\setlength\\LTright{0pt} \\begin{longtable}{@{\extracolsep{\\fill}}rlll@{}}
+    print("""\\textbf{%s UG Curriculum}"""%deptname)
+    table_pre = """\\begin{longtable}{@{}rlll@{}} \
+            \\textcolor{Grey}{\\textbf{Cred.}} & \\textcolor{Grey}{\\textbf{Code}} & \
+    \\textcolor{Grey}{\\textbf{Course Title}} & \\textcolor{Grey}{\\textbf{Segments}} \\\\ \
+    \\toprule \\endhead"""
     print(table_pre)
     row_style = "\\currformat{%s}{%s}{%s}{%s}"
     q = """SELECT credits, segments, name FROM %s_courses WHERE code='%s'"""
     sem, t1, t2 = "0", 0, 0 # t1 = total credits, t2 = credits for a semester.
     for r in sheet.iter_rows(min_row=2):
         if sem != r[0].value:
-            if sem != "0": print(" & Total & %d & \\\\"%t2)
+            if sem != "0": print("\\textbf{%d} &  & \\\\"%t2)
+            t1 += t2
             sem, t2 = r[0].value, 0
-            print(" & Semester %s & & \\\\" % sem)
+            print(" & \\textbf{Sem. %s} & & \\\\" % sem)
         code = r[1].value
-        dept = code[:2].lower()
+        dept = code[:2].upper()
         #print(code, dept)
         try: credits, segments, name = cur.execute(q % (dept, code)).fetchall()[0]
-        except: continue
+        except: 
+            #print(r[3].value, r[4].value, r[2].value)
+            credits, segments, name = int(r[3].value), r[4].value, r[2].value
         t1 += int(credits)
         t2 += int(credits)
         if segments == None or segments == 'None': segments = ''
         name = name.replace('&', 'and')
-        print(row_style % (code, name, credits, segments))
+        #print(row_style % (code, name.title(), credits, segments))
+        print(row_style % (credits, code, name.title(), segments))
+    print("\\textbf{%d} &  & \\\\"%t2)
+    #print("\\textbf{%d} & Grand Total & \\\\"%t1)
     table_post = "\\end{longtable}"
     print(table_post)
-    print("".join(post))
+    #print("".join(post))
 
+def print_part(f): print(''.join(open(f).readlines()))
 
 import sys
 if __name__ == "__main__":
+    def get_deptname(f): return f.split('-')[0].split('/')[-1].upper()
     if len(sys.argv) > 1:
         if sys.argv[1] == "update":
-            wb = load_workbook(sys.argv[2])
-            desc = wb.get_sheet_by_name("course-descriptions")
-            update_dept_cdesc(sys.argv[3], sheet)
+            for x in course_details_filelist:
+                #print(x)
+                deptname = get_deptname(f) 
+                wb = load_workbook(x)
+                desc = wb.get_sheet_by_name("course-descriptions")
+                update_dept_cdesc(deptname, desc)
         elif sys.argv[1] == "print-desc":
+            print_part('./parts/pre-doc.tex')
             gen_course_description(sys.argv[2])
+            print_part('./parts/post-doc.tex')
         elif sys.argv[1] == "print-curr":
-            wb = load_workbook("%s.xlsx" % sys.argv[2])
+            wb = load_workbook(sys.argv[2])
             sheet = wb.get_sheet_by_name("curriculum")
-            gen_curriculum(sys.argv[2], sheet, "UG Curriculum")
+            deptname = get_deptname(sys.argv[2]) 
+            print_part('./parts/pre-doc.tex')
+            gen_curriculum(deptname, sheet, "UG Curriculum")
+            print_part('./parts/post-doc.tex')
+        elif sys.argv[1] == "print-doc":
+            print_part('./parts/pre-doc.tex')
+            dlist = [
+                    './data/UG/CS-BTech.xlsx',
+                    './data/UG/EE-BTech.xlsx',
+                    './data/UG/BT-BTech.xlsx']
+                    #'./data/UG/CS-BTech.xlsx', 
+                    #'./data/UG/CHE-BTech.xlsx', 
+                    #'./data/UG/EE-BTech.xlsx', 
+            for f in dlist:
+                deptname = get_deptname(f) 
+                wb = load_workbook(f)
+                sheet = wb.get_sheet_by_name("curriculum")
+                gen_curriculum(deptname, sheet, "UG Curriculum")
+            for f in dlist:
+                deptname = get_deptname(f) 
+                gen_course_description(deptname)
+            print_part('./parts/post-doc.tex')
     
     
 
